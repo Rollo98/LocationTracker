90759d4db643ac92325cdd7a6974d03dcceaf823
Rolleh <32469870+Rollo98@users.noreply.github.com>
2019-03-09 00:49:05 +0200

    working on it

---
+++ .history/App_20190309003254.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import { Text, View, StyleSheet } from "react-native";
+import { Constants, Location, Permissions } from "expo";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      locationResult: null,
+      latitude: undefined,
+      longitude: undefined,
+      routeCoordinates: [],
+      distanceTravelled: 0,
+      prevLatLng: {}
+    };
+  }
+
+  componentDidMount() {
+    this._getLocationAsync();
+  }
+
+  _getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== "granted") {
+      this.setState({
+        locationResult: "Permission to access location was denied"
+      });
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    this.setState({ locationResult: JSON.stringify(location) });
+  };
+
+  render() {
+    return (
+      <View>
+        <Text>
+          {"\n"}Location: {this.state.locationResult}
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: "#ecf0f1"
+  }
+});
---
+++ .history/index_20190309002001.js
@@ -0,0 +1,4 @@
+import { AppRegistry } from "react-native";
+import asd from "./App";
+
+AppRegistry.registerComponent("LocationTracker", () => App);
