44642080ebb631a9e0bf87f1b85792e282570051
Rolleh <32469870+Rollo98@users.noreply.github.com>
2019-05-07 08:30:27 +0300

    fixed issues

--- .history/App_20190309011917.js
+++
@@ -1,2 +0,0 @@
-import { React } from "react";
-import MapView from "react-native-maps";
--- .history/index_20190309002001.js
+++
@@ -1,4 +0,0 @@
-import { AppRegistry } from "react-native";
-import asd from "./App";
-
-AppRegistry.registerComponent("LocationTracker", () => App);
